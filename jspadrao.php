e7b38ee6d0b5deefda654c9817d514eb58caf482
danielmendesm <dasilvamendes.daniel@gmail.com>
2021-08-28 16:19:11 -0400

    Formulário de login
    Desenvolvimento do layout da tela de login.

---
+++ jspadrao.php
@@ -0,0 +1,7 @@
+        <!-- jQuery -->
+        <script src="templates/AdminLTE-3.1.0//plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="templates/AdminLTE-3.1.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="templates/AdminLTE-3.1.0/dist/js/adminlte.min.js"></script>
+        <script src="=templates/AdminLTE-3.1.0/plugins/jquery-validation/jquery.validate.in.js"></script>
---
+++ login.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Icones do Projeto (Font-Awesome) -->
+  <link rel="stylesheet" href="templates/AdminLTE-3.1.0/plugins/fontawesome-free/css/all.min.css">
+  <!-- Estilo das caixas de seleção -->
+  <link rel="stylesheet" href="templates/AdminLTE-3.1.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Estilo do template Admin LTDE -->
+  <link rel="stylesheet" href="templates/AdminLTE-3.1.0/dist/css/adminlte.min.css">
+  <title>Autenticação</title>
+</head>
+
+<body class="hold-transition login-page">
+  <div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+      </div>
+      <div class="card-body">
+      <div class="callout callout-warning">
+                  <h5>Atenção! </h5>
+
+                  <p>Todos os campos sinalizados com ** são obrigatorios.</p>
+                </div>
+
+        <form action="../../index3.html" method="post">
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Digite pelo seu celular ou E-mail *">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Digite sua senha *">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <!-- /.col -->
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+        <p class="mb-2">
+          <a href="register.html" class="text-center">Criar um novo usuário</a>
+        </p>
+        <p class="mb-0">
+          <a href="register.html" class="text-center">Esqueci a Senha</a>
+        </p>
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+  </div>
+
+
+
+
+  <!-- Biblioteca JavaScript -->
+  <!-- jQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"></script>
+</body>
+
+</html>
